5754413c6b8f004e57564ee5578126a070bd08ea
Angel Leon <gubatron@gmail.com>
2023-06-13 10:54:48 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Sample Swing App with an embedded JavaFX Web Browser component
+
+This seems to be the easiest and free path if you need to embed a chrome browser in your java app.
+
+The build.gradle file points to a fixed location on my macbook, might want to change that if you want to build.
+
+## Gradle tasks
+
+### Build
+`./gradlew build`
+
+### Run
+`./gradlew run`
+
+### Clean
+`./gradlew clean`
---
+++ src/com/gubatron/Main.java
@@ -0,0 +1,42 @@
+package com.gubatron;
+
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Scene;
+import javafx.scene.web.WebView;
+import javax.swing.*;
+
+public class Main {
+    private static void initAndShowGUI() {
+        // This method is invoked on Swing thread
+        JFrame frame = new JFrame("FX");
+        final JFXPanel fxPanel = new JFXPanel();
+        frame.add(fxPanel);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                initFX(fxPanel);
+            }
+        });
+    }
+
+    private static void initFX(JFXPanel fxPanel) {
+        // This method is invoked on JavaFX thread
+        WebView webView = new WebView();
+        webView.getEngine().load("http://google.com");
+        fxPanel.setScene(new Scene(webView));
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                initAndShowGUI();
+            }
+        });
+    }
+}
